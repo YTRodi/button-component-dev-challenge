6339fa49f361d7eefe3696629b50c448f1f40eea
YTRodi <yagorodi@gmail.com>
2022-10-30 14:27:05 -0300

    Initial commit

---
+++ apps/button-component-dev-challenge-e2e/src/e2e/app.cy.ts
@@ -0,0 +1,13 @@
+import { getGreeting } from '../support/app.po';
+
+describe('button-component-dev-challenge', () => {
+  beforeEach(() => cy.visit('/'));
+
+  it('should display welcome message', () => {
+    // Custom command example, see `../support/commands.ts` file
+    cy.login('my-email@something.com', 'myPassword');
+
+    // Function helper example, see `../support/app.po.ts` file
+    getGreeting().contains('Welcome button-component-dev-challenge');
+  });
+});
