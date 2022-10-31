e0caff5aa7f6c0aa96553ccb48b7ce47186acc4c
YTRodi <yagorodi@gmail.com>
2022-10-31 12:44:58 -0300

    feat: WIP Button UI component

--- libs/ui/src/lib/button/button.tsx
+++ libs/ui/src/lib/button/button.tsx
@@ -1,13 +1,74 @@
-import styles from './button.module.scss';
+import React from 'react';
+import { cva, type VariantProps } from 'class-variance-authority';
 
-/* eslint-disable-next-line */
-export interface ButtonProps {}
+const buttonStyles = cva(
+  [
+    'font-noto-sans-jp',
+    'rounded-md',
+    'transition-colors',
+    'bg-slate',
+    'text-slate-darkest',
+    'hover:bg-slate-dark',
+    'focus:bg-slate-dark',
+    'focus:outline-none',
+  ],
+  {
+    variants: {
+      // variant: {
+      //   default: '', // or SOLID
+      //   outline: ['bg-transparent', 'border', 'border-mine-shaft'],
+      //   text: ['bg-transparent'],
+      // },
+      color: {
+        default: ['focus:outline-slate-darkest'],
+        primary: [
+          'bg-blue',
+          'text-white',
+          'hover:bg-blue-dark',
+          'focus:bg-blue-dark',
+          'focus:outline-blue-dark',
+        ],
+        secondary: [
+          'bg-gray',
+          'text-white',
+          'hover:bg-gray-dark',
+          'focus:bg-gray-dark',
+          'focus:outline-gray-dark',
+        ],
+        danger: [
+          'bg-red',
+          'text-white',
+          'hover:bg-red-dark',
+          'focus:bg-red-dark',
+          'focus:outline-red-dark',
+        ],
+      },
+      size: {
+        sm: ['py-[6px]', 'px-3'],
+        md: ['py-2', 'px-4'],
+        lg: ['py-[11px]', 'px-[22px]'],
+      },
+      full: {
+        true: 'w-full',
+      },
+    },
+    defaultVariants: {
+      color: 'default',
+      size: 'md',
+      full: false,
+    },
+  }
+);
 
-export function Button(props: ButtonProps) {
+export interface ButtonProps
+  extends Omit<React.ButtonHTMLAttributes<HTMLButtonElement>, 'color'>,
+    VariantProps<typeof buttonStyles> {}
+
+export function Button({ children, color, size, full, ...props }: ButtonProps) {
   return (
-    <div className={styles['container']}>
-      <h1>Welcome to Button!</h1>
-    </div>
+    <button className={buttonStyles({ color, size, full })} {...props}>
+      {children}
+    </button>
   );
 }
 
